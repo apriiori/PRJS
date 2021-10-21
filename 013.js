0fea5a3d99d1c0c1f0cac6fb74ea10649d36ef4d
dima <skymaxnet@yandex.ru>
2021-10-21 13:58:30 +0600

    21.10

---
+++ 013.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const num = 55;
+
+switch (num) {
+    case 49:
+        console.log('неверно');
+        break;
+    case 100:
+        console.log('неверно');
+        break;
+    case 50:
+        console.log('В точку');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
